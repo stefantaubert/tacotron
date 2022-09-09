a1b59ef8fd36a7a9b8f017f9191b1727869f3ddb
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-09-09 14:01:27 +0200

    add tone detection method

---
+++ src/tacotron_tests/test_tone_detection.py
@@ -0,0 +1,25 @@
+from tacotron.tone_detection import split_phoneme_and_tone
+
+
+def test_vowel_toned__returns_vowel_tone():
+  phoneme, tone = split_phoneme_and_tone("i˧˩˧")
+  assert phoneme == "i"
+  assert tone == "˧˩˧"
+
+
+def test_diphthong_toned__returns_vowel_tone():
+  phoneme, tone = split_phoneme_and_tone("ai˩˧")
+  assert phoneme == "ai"
+  assert tone == "˩˧"
+
+
+def test_consonant__returns_consonant_empty():
+  phoneme, tone = split_phoneme_and_tone("b")
+  assert phoneme == "b"
+  assert tone == ""
+
+
+def test_consonant_toned__returns_consonant_empty():
+  phoneme, tone = split_phoneme_and_tone("b˧")
+  assert phoneme == "b"
+  assert tone == "˧"
